99c3503fcc7c1e1a78e19127d8350245ae675c4f
Kareky <riccardo.morosin@hotmail.com>
2025-12-17 20:03:56 +0100

    Testing Split and Join

--- Tutorials/ArrayMethods/Program.cs
+++ Tutorials/ArrayMethods/Program.cs
@@ -44,8 +44,21 @@ foreach (var pallet in pallets)
     Console.WriteLine($"-- {pallet}");
 }*/
 
-string value = "abc123";
+/*string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
 string result = new string(valueArray);
+Console.WriteLine(result);*/
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
